b509bf1444b43f8701d1960494d1ed612feef3c3
Yakubu Zakaria <78931393+zakjak@users.noreply.github.com>
2026-04-13 03:57:41 -0500

    fixed the same setOpen error

--- components/LatestArticles.tsx
+++ components/LatestArticles.tsx
@@ -3,22 +3,16 @@
 import { useLatestStories } from "@/hooks/useLatestStories";
 import LatestArticlesSkeleton from "./LatestArticlesSkeleton";
 import TopCategoryStory from "./TopCategoryStory";
-import { useState } from "react";
 import { Article } from "@/lib/types/article";
 
 const LatestArticles = () => {
   const { data: articles, isLoading } = useLatestStories();
-  const [open, setOpen] = useState(false);
 
   if (isLoading) return <LatestArticlesSkeleton />;
   return (
     <div className="grid gap-4 lg:grid-cols-3 md:grid-cols-2">
       {articles?.map((article: Article) => (
-        <TopCategoryStory
-          key={article?.id}
-          topStory={article}
-          setOpen={setOpen}
-        />
+        <TopCategoryStory key={article?.id} topStory={article} />
       ))}
     </div>
   );
