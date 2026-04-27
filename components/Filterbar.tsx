8adb0799d3b29296dd0e616c5132a8f533c48771
Yakubu Zakaria <78931393+zakjak@users.noreply.github.com>
2026-04-26 19:47:11 -0500

    commented out searchinput in dashboard

--- components/Filterbar.tsx
+++ components/Filterbar.tsx
@@ -2,11 +2,7 @@ import React from "react";
 import SearchInput from "./SearchInput";
 
 const Filterbar = () => {
-  return (
-    <div className="">
-      <SearchInput />
-    </div>
-  );
+  return <div className="">{/* <SearchInput /> */}</div>;
 };
 
 export default Filterbar;
